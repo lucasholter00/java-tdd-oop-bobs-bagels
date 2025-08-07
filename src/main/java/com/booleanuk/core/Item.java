dabc17d35ca2bd6656d73d77585f0f9f44f9eb93
Lucas Holter <lucas.holter@hotmail.com>
2025-08-07 16:24:44 +0200

    Add attributes as well as getters

---
+++ src/main/java/com/booleanuk/core/Item.java
@@ -0,0 +1,31 @@
+package com.booleanuk.core;
+
+public class Item {
+    private String sku;
+    private float price;
+    private String name;
+    private String variant;
+
+    public Item(String sku, float price, String name, String variant){
+        this.sku = sku;
+        this.price = price;
+        this.name = name;
+        this.variant = variant;
+    }
+
+    public String getSku(){
+        return this.sku;
+    }
+
+    public float getPrice(){
+        return this.price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getVariant(){
+        return this.variant;
+    }
+}
